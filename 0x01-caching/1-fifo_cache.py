ff027c1f9221acd386d946c990a924990cd2d921
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-02 02:54:03 +0100

    Testing again

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -10,12 +10,12 @@ class FIFOCache(BaseCaching):
         """Puts an item in cache with key key"""
         if key is None or item is None:
             return
-        if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_key = [key for key in self.cache_data][0]
             print("first_key", first_key)
             del self.cache_data[first_key]
             print(f"DISCARD: {first_key}")
-        self.cache_data[key] = item
 
     def get(self, key):
         """Gets an item from cache using key"""
