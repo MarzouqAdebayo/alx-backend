b0f9a779bc0b976d42068239c12c25f8467a99fa
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-09 03:05:20 +0100

    :bug: fixed template name in task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -5,14 +5,6 @@ from flask_babel import Babel
 from typing import Union, Dict
 
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
-
 class Config:
     """Babel config"""
 
@@ -25,6 +17,12 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config.from_object(Config)
 babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 def get_user() -> Union[Dict, None]:
@@ -54,7 +52,7 @@ def get_locale() -> str:
 @app.route("/")
 def get_index() -> str:
     """The home page route handler"""
-    return render_template("3-index.html")
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":
