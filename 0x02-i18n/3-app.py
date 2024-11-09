8d398eb6a143405f0a0071c52847d8ef71f45f21
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-09 02:17:42 +0100

    :bug: fixed error in task 2

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -27,7 +27,7 @@ def get_locale() -> str:
 @app.route("/")
 def get_index() -> str:
     """The home page route handler"""
-    return render_template("2-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
